b729fea68004c374c367bd59e57e3d5fc69a330c
Bohdan Baran <bohdanbrn@gmail.com>
2017-01-02 18:22:21 +0200

    centralmedia ver. 0.3

---
+++ wp-content/themes/centralmedia/Ajax_Poll/poll-simple/css/style.inc.php
@@ -0,0 +1,262 @@
+<?php
+//==>>>==>>>==>>>==>>>==>>>==>>>==>>>==>>>==>>>==>>>==>>>==>>>==>>>
+//
+// Ajax Poll Script v3.18 [ GPL ]
+// Copyright (c) phpkobo.com ( http://www.phpkobo.com/ )
+// Email : admin@phpkobo.com
+// ID : APSMX-318
+// URL : http://www.phpkobo.com/ajax_poll.php
+//
+//==<<<==<<<==<<<==<<<==<<<==<<<==<<<==<<<==<<<==<<<==<<<==<<<==<<<
+
+//----------------------------------------------------------------
+// <style>
+//----------------------------------------------------------------
+$style=<<<_EOM_
+<style>
+/* [BEGIN] CSS Reset */
+.%tclass% tbody,
+.%tclass% tfoot,
+.%tclass% thead,
+.%tclass% tr,
+.%tclass% th,
+.%tclass% td {
+	margin:0;
+	padding:0;
+	border:0;
+	font-size:100%;
+	font-style:normal;
+	font-weight:normal;
+	vertical-align:middle;
+	background-color:transparent;
+	box-shadow:none;
+}
+.%tclass% img {
+	margin:0;
+	padding:0;
+	border:0;
+	background-color:transparent;
+	box-shadow:none;
+}
+.%tclass% table {
+}
+.%tclass% label {
+	margin:2px 0;
+	font-style:normal;
+	font-weight:normal;
+}
+.%tclass% button {
+	color:black;
+}
+/* [END] CSS Reset */
+
+.%tclass%,
+.%tclass% button {
+	font-family: "Verdana", "Arial", "Helvetica", "serif";
+}
+
+.%tclass% {
+	box-sizing:content-box;
+	position:relative;
+	overflow:hidden;
+}
+
+.%tclass% .poll-inner {
+	position:relative;
+
+	margin:20px;
+	margin-bottom:0;
+	padding:0;
+	font-size:16px;
+	color:black;
+	letter-spacing:normal;
+	line-height: normal;
+	word-spacing: 0px;
+}
+
+.%tclass% .poll-form {
+	margin:0;
+}
+
+.%tclass% .poll-title {
+	margin-bottom:20px;
+	padding:0;
+	text-align: left;
+
+	color:black;
+	font-weight:bold;
+	font-size:16px;
+	line-height: 0px;
+}
+
+.%tclass% .poll-table {
+	border-collapse:collapse;
+}
+
+.%tclass% .poll-table tr {
+}
+
+.%tclass% .poll-table td {
+	margin:0;
+	padding:0;
+	font-size:16px;
+	color:black;
+	*border:1px solid transparent;
+	vertical-align:middle;
+}
+
+.%tclass% .poll-table td.poll-caption-cont {
+	padding:10px;
+	padding-right:0;
+}
+
+.%tclass% .poll-bar {
+	display:none;
+	width:0;
+	height:12px;
+	background-color:rgba(216, 45, 25, 0.8);
+
+	-moz-border-radius: 4px;
+	-webkit-border-radius: 4px;
+	-khtml-border-radius: 4px;
+	border-radius: 4px;
+}
+
+.%tclass% .poll-button {
+	margin:0 0 3px 0;
+	font-size:16px;
+	width:140px;
+}
+
+.%tclass% .poll-button:focus {
+	outline: 0;
+}
+
+.%tclass% .poll-time-msg {
+	color:#888;
+	text-align:center;
+	font-weight:bold;
+	margin-bottom:20px;
+	padding:3px 0;
+
+	border:1px solid #888;
+	-moz-border-radius: 3px;
+	-webkit-border-radius: 3px;
+	-khtml-border-radius: 3px;
+	border-radius: 3px;
+}
+
+.%tclass% .ap-clear-block {
+	position:absolute;
+	right:15px;
+	top:0px;
+	height:18px;
+	color:black;
+	font-size:12px;
+	background-color:#eee;
+	border:1px solid #aaa;
+	border-radius:4px;
+	cursor:pointer;
+	display: none;
+}
+
+/*-- tipbox --*/
+.%tclass% .poll-tipbox {
+	box-sizing:content-box;
+	z-index:10000;
+	display:none;
+	position:absolute;
+	top:0;
+	left:0;
+	margin:0;
+	padding:0
+	background-color:yellow;
+}
+.%tclass% .poll-tipbox .poll-tipbox-selectone,
+.%tclass% .poll-tipbox .poll-tipbox-havevoted,
+.%tclass% .poll-tipbox .poll-tipbox-thankyou {
+	margin:0;
+	padding:10px 15px;
+
+	text-align:center;
+	font-size:16px;
+	font-weight:normal;
+	font-style:normal;
+
+	color:#fff;
+	background-color:#080;
+	border:0 solid #cfcfcf;
+	border-width:0px 0;
+
+	-moz-box-shadow: 1px 1px 3px #000;
+	-webkit-box-shadow: 1px 1px 3px #000;
+}
+.%tclass% .poll-tipbox .poll-tipbox-selectone {
+	background-color:#03A9F4;
+}
+.%tclass% .poll-tipbox .poll-tipbox-havevoted {
+	background-color:#03A9F4;
+}
+.%tclass% .poll-tipbox .poll-tipbox-thankyou {
+	background-color:#03A9F4;
+}
+
+/*-- checkbox --*/
+.%tclass% .poll-input {
+	display:none;
+}
+.%tclass% .poll-input-cont {
+	position:relative;
+	display:inline-block;
+	*zoom:1;
+	*display:inline;
+	margin:0;
+	padding:0;
+	width:35px;
+	height:35px;
+	border-radius:5px;
+	vertical-align:middle;
+	text-align:center;
+	cursor:pointer;
+	background-color:#eee;
+}
+.%tclass% .poll-input-inner {
+	margin:10%;
+	padding:0;
+	width:80%;
+	height:80%;
+	background-color:#eee;
+	border-radius:2px;
+}
+.%tclass% .poll-input-cont-on,
+.%tclass% .poll-input-inner-on {
+	//background-color:#888;
+}
+.%tclass% .poll-input-mark {
+	width:100%;
+	height:100%;
+	visibility:hidden;
+}
+.%tclass% .poll-input-mark-on {
+	visibility:visible;
+}
+.%tclass% .poll-input-img {
+	display:none;
+}
+
+.%tclass% .btn,
+.%tclass% .btn-large {
+    text-decoration: none;
+    color: #fff;
+    background-color: #d82d19;
+    text-align: center;
+    letter-spacing: .5px;
+    transition: .2s ease-out;
+    cursor: pointer;
+}
+
+</style>
+_EOM_;
+$this->addStyle($style);
+
+?>
---
+++ wp-content/themes/centralmedia/home-content.php
@@ -0,0 +1,287 @@
+<!--home-content-->
+
+<div style="height: 58px;"></div>
+<div class="row slider-section">
+	<!--slider-->
+	<div class="col l8 s12 m12">
+		<div class="carousel carousel-slider center" data-indicators="true">
+			<div class="carousel-fixed-item center">
+				<a class="btn waves-effect slider-button white-text darken-text-2">Перейти</a>
+			</div>
+			<div class="carousel-item slide-1 white-text" href="#one!">
+				<h2 class="slider-sign-main">ТОП-5 Новин</h2>
+				<p class="white-text slider-sign-small container">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+			</div>
+			<div class="carousel-item slide-2 white-text" href="#two!">
+				<h2 class="slider-sign-main">ТОП-5 Новин</h2>
+				<p class="white-text slider-sign-small container">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+			</div>
+			<div class="carousel-item slide-1 white-text" href="#three!">
+				<h2 class="slider-sign-main">ТОП-5 Новин</h2>
+				<p class="white-text slider-sign-small container">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+			</div>
+			<div class="carousel-item slide-2 white-text" href="#four!">
+				<h2 class="slider-sign-main">ТОП-5 Новин</h2>
+				<p class="white-text slider-sign-small container">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.</p>
+			</div>
+		</div>
+	</div>
+
+	<!--Blogs-->
+	<div class="col l4 s12 m12">
+		<div class="blogs">
+			<span class="blog-sign">БЛОГИ </span>
+
+			<?php 
+			 $args = array(
+		                'post_type' => 'blogs',
+		                'posts_per_page' => 3,
+		                'publish' => true,
+		                'orderby' => 'date',
+		                'order' => 'DESC'
+		            );
+            $myposts = get_posts( $args );
+
+            function short_desc_blog($charlength) {		// function for display short content for blogs
+				$excerpt = get_the_excerpt();
+				?>
+				<a href="<?php the_permalink(); ?>" class="blog-short-desc" >
+				<?php
+					if ( mb_strlen( $excerpt ) > $charlength ) {
+						$subex = mb_substr( $excerpt, 0, $charlength);
+						echo $subex . '...';
+					} else {
+						echo $subex;
+					}
+					?>
+				</a>
+			<?php
+			} // end function short_desc_blog()
+            foreach( $myposts as $post ){
+            	setup_postdata($post);
+        ?>
+		<div class="blog-block row">
+			<div class="blogger-photo col l3 s1 center" >
+				<img src="<?php bloginfo('template_url') ?>/img/users/user2.png" alt="user">
+			</div> 
+			<div class="blogger-article col l9 s11">
+				<div class="blogger-name"><?php the_author(); ?></div>
+				<div class="blogger-speach">
+					<?php
+						short_desc_blog(100);		// display short content
+					?>
+				</div>
+			</div>
+		</div>
+        <?php
+    		} /* end foreach */
+			wp_reset_postdata();
+		?>
+		</div>
+	</div>
+</div>
+
+<!--news-->
+<div class="row content">
+	<div class="col l6 s12 m6 news-block ">
+		<div class="news-sign center">ОСТАННІ НОВИНИ</div>
+		
+		<?php 
+			 $args = array(
+		                'post_type' => 'news',
+		                'posts_per_page' => 10,
+		                'publish' => true,
+		                'orderby' => 'date',
+		                'order' => 'DESC'
+		            );
+            $myposts = get_posts( $args );
+
+            foreach( $myposts as $post ){
+            	setup_postdata($post);
+        ?>
+
+		<div class="article"> 
+			<div class="article-title">
+				<a href="<?php the_permalink(); ?>" class="hover-link"><i class="fa fa-bar-chart" aria-hidden="true"></i>
+					<?php the_title(); ?>
+				</a>
+			</div>
+		</div>
+        <?php
+    		} /* end foreach */
+			wp_reset_postdata();
+		?>
+		
+		<div class="article"> 
+			<div class="article-title">
+				<a href="#" class="hover-link"><i class="fa fa-circle-o-notch fa-spin" aria-hidden="true"></i>
+					Путін заявив, що Росія сильніша за будь-якого агресора
+				</a>
+			</div>
+		</div>
+		<div class="article"> 
+			<div class="article-title">
+				<a href="#" class="hover-link red-text "><em>фото</em>ДЕПУТАТ У ДНІПРІ ПОПАВСЯ НА ХАБАРІ</a>
+			</div>
+		</div>
+		<div class="article"> 
+			<div class="article-title">
+				<a href="#" class="hover-link"><em>фото</em>В Росії цензура не пропустила три книги для Сущенка</a>
+			</div>
+		</div>
+		
+	</div>
+
+	<!--Videos-->
+	<div class="col l3 s12 m6 center video-block">
+		<div class="news-sign-video center">ВІДЕО</div>
+		<div class="video-list row">
+			<div class="youtube col l6">
+				<img width="80%" src="<?php bloginfo('template_url'); ?>/img/logo/youtube-icon.png" alt="альтернативный текст" />
+				<div class="center">Канал YouTube</div>
+			</div>
+			<div class="youtube col l6">
+				<img  width="80%" src="<?php bloginfo('template_url'); ?>/img/logo/youtube-icon-stream.png" alt="альтернативный текст" />
+				<div class="center">Онлайн трансляція</div>
+			</div>
+		</div>
+
+		<div class="top-video">Топ-5 Відео</div>
+		<?php 
+			 $args = array(
+		                'post_type' => 'video',
+		                'posts_per_page' => 5,
+		                'publish' => true,
+		                'orderby' => 'date',
+		                'order' => 'DESC'
+		            );
+            $myposts = get_posts( $args );
+            foreach( $myposts as $post ){
+            	setup_postdata($post);
+        ?>
+		<div class="row video-padding">
+			<div class="youtube-top-video col l4">
+				<img width="80%" src="<?php video_thumbnail(); ?>" alt="альтернативный текст" />
+			</div>
+			<div class="video-text col l8">
+				<a href="<?php the_permalink(); ?>" class="hover-link">
+					<?php the_title(); ?>
+				</a>
+			</div>
+		</div>
+        <?php
+    		} /* end foreach */
+			wp_reset_postdata();
+		?>
+	</div>
+
+	<!--state-->
+	<div class="col l3 s12 m6 center state-block">
+		<div class="state-list">
+			<div class="state-sign center">СТАТТІ</div>
+			<div class="blogs">
+				<?php 
+					$args = array(
+		                'post_type' => 'articles',
+		                'posts_per_page' => 3,
+		                'publish' => true,
+		                'orderby' => 'date',
+		                'order' => 'DESC',
+		                'paged' => get_query_var('paged')
+		            );
+		            $firstArticle = false;
+		            $myposts = get_posts( $args );
+
+		            function short_desc_article($charlength) {		// function for display short content for states
+						$excerpt = get_the_excerpt();
+						?>
+						<?php
+							if ( mb_strlen( $excerpt ) > $charlength ) {
+								$subex = mb_substr( $excerpt, 0, $charlength);
+								echo $subex . '...';
+							} else {
+								echo $subex;
+							}
+					}
+
+					foreach( $myposts as $post ){
+						setup_postdata($post);
+						if ($firstArticle == false) {
+				?>
+				<div class="row main-state">
+					<div class="col s12 m12 l12">
+						<img src="<?php the_post_thumbnail_url(); ?>" width="100%" alt="">
+						<a href="<?php the_permalink(); ?>" class="hover-link">
+							<span class="state-first-name"><?php the_title(); ?></span>
+						</a>
+						<div>
+							<?php
+								short_desc_article(120);		// display short content
+							?>
+						</div>
+					</div>
+				</div>
+				<?php
+					} /* end if */
+					else{
+				?>
+				<div class="row other-state">
+					<div class="col s12 m12 l12">
+						<img src="<?php the_post_thumbnail_url(); ?>" width="40%" alt="" class="state-other-float">
+						<a href="<?php the_permalink(); ?>" class="hover-link">
+							<div class="state-other-name"><?php the_title(); ?></div>
+						</a>
+						<div class="state-other-text">
+							<?php
+								short_desc_article(120);
+							?>
+						</div>
+					</div>
+				</div>
+
+				<?php
+						} /* end else */
+					$firstArticle = true;
+					} /* end foreach */
+					wp_reset_postdata();
+		        ?>		
+			</div>
+		</div>
+	</div>
+</div>
+<br>
+
+<!--polling-->
+<div class="row polling">
+	<div class="col l4 s12 m6 asking-block">
+		<h5 class="asking">Опитування</h5>
+		<div class='ajax-poll' tclass='poll-simple' style='width:450px;'></div>
+	</div>
+	<!--partners news-->
+	<div class="col l4 s12 m6 news-partners-block">
+		<h5 class="partners-news center">Новини партнерів</h5>
+		<div class="article"> 
+	<div class="article-title">
+		<a href="#" class="hover-link"><i class="fa fa-bar-chart" aria-hidden="true"></i>
+			Активісти на авто з єврономерами перекрили центр Києва</a>
+		</div>
+	</div>
+	<div class="article"> 
+		<div class="article-title">
+			<a href="#" class="hover-link"><i class="fa fa-circle-o-notch fa-spin" aria-hidden="true"></i>
+				Путін заявив, що Росія сильніша за будь-якого агресора</a>
+			</div>
+
+		</div>
+		<div class="article"> 
+			<div class="article-title">
+				<a href="#" class="hover-link red-text "><em>фото</em>ДЕПУТАТ У ДНІПРІ ПОПАВСЯ НА ХАБАРІ</a>
+			</div>
+
+		</div>
+	</div>
+	<!--advertising-->
+	<div class="col l4 s12 m6 center reklama">
+		<img src="<?php bloginfo('template_url'); ?>/img/logo/logo_kodeks.png" width="40%">
+	</div>
+</div>
