5029d0ef0d692aa1b4065744259590dea53a87b6
Yura Romaniv <romaniv.yura95@gmail.com>
2017-01-17 15:13:19 +0200

    redesign search page

--- wp-content/themes/centralmedia/search.php
+++ wp-content/themes/centralmedia/search.php
@@ -1,16 +1,19 @@
 <?php get_header(); ?>
 	
-	<section id="primary" class="content-area">
+	<section id="primary" class="content-area container">
 		<main id="main" class="site-main" role="main">
 			
-			<h3> Ви шукали "<?php the_search_query(); ?>" </h3>
-			<h4> Результати пошуку: </h4>
+			<span class="search-request"> Ви шукали "<?php the_search_query(); ?>" </span>
+
+			<div class="search-result"> Результати пошуку: </div>
+			<div class="search-post-number"> За Вашим запитом знайдено 7845 записів.</div>
 		<?php 
 				if ( have_posts() ) :
 					while ( have_posts() ) : the_post(); // Start the Loop.
 			?>
 					<div class="entry-summary">
-						<a href="<?php the_permalink(); ?>">
+					<div class="search-post-title"><a href="<?php the_permalink(); ?>" class="hover-link"> <?php the_title(); ?></a> </div>
+						<a href=" <?php the_permalink(); ?>">
 							<?php the_excerpt(); ?>
 						</a>
 					</div>
@@ -20,7 +23,7 @@
 					the_posts_pagination( $pagination_args );
 					wp_reset_postdata();
 				else :
-					echo '<div class=""> За вашим запитом нічого не знайдено </div>';
+					echo '<div class="search-null"> За Вашим запитом нічого не знайдено. </div>';
 				endif;
 		?>
 
